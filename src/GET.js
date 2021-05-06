24ac9399a029017f597de6a91827861e2512c37f
181830146YichenOuyang <181830146@smail.nju.edu.cn>
2021-05-06 23:26:59 +0800

    update notes

--- src/GET.js
+++ src/GET.js
@@ -2,18 +2,17 @@ import store from "./store"
 import type from "./store/type"
 import axios from 'axios';
 
-export default function GET()
-{
-    // const _this=this; 
-    
-    axios.get("http://localhost:8181/api/GET")
-    .then(function(resp){
+export default function GET() {
+  // const _this=this; 
 
-       let action={//定义动作
-        type:type.INITIALIZE,
+  axios.get("http://localhost:8181/api/GET")
+    .then(function (resp) {
+
+      let action = {//定义动作
+        type: type.INITIALIZE,
         payload: resp.data.data
       }
-      
+
       store.dispatch(action);
 
     })
@@ -21,16 +20,17 @@ export default function GET()
       console.log(error);
     });
 
-    // save_data=[
-    //   {
-    //   key: '1',
-    //   firstName: 'John',
-    //   lastName: 'Brown',
-    //   age: 32,
-    //   address: 'New York No. 1 Lake Park',
-    //   tags: ['nice', 'developer'],
-    //   }
-    // ]
-    
-    
+  // test
+  // save_data=[
+  //   {
+  //   key: '1',
+  //   firstName: 'John',
+  //   lastName: 'Brown',
+  //   age: 32,
+  //   address: 'New York No. 1 Lake Park',
+  //   tags: ['nice', 'developer'],
+  //   }
+  // ]
+
+
 }
